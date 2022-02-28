4572ca14e37a704aed576cbe791029327bef2504
Guru Karthikeyan <gurukartkn@gmail.com>
2022-02-28 16:52:05 +0530

    routes commit

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,5 +1,17 @@
 import React from "react";
+import { Routes as Router, Route, Navigate } from "react-router-dom";
+import { Results } from "./Results";
 
 export const Routes = () => {
-  return <div>Routes</div>;
+  return (
+    <div className="p-4">
+      <Router>
+        <Route exact path="/" element={<Navigate replace to="/search" />} />
+        <Route exact path="/search" element={<Results />} />
+        <Route exact path="/images" element={<Results />} />
+        <Route exact path="/news" element={<Results />} />
+        <Route exact path="/videos" element={<Results />} />
+      </Router>
+    </div>
+  );
 };
